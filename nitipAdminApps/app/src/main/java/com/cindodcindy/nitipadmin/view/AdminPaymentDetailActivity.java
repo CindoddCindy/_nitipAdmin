f3ac429cc919a7197262dc16dc4401c4c2fc732d
CindoddCindy <cindodcindy@gmail.com>
2021-04-23 15:40:52 +0700

    tambah done item dan done detail

---
+++ nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/AdminPaymentDetailActivity.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.nitipadmin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.nitipadmin.R;
+
+public class AdminPaymentDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_payment_detail);
+    }
+}
