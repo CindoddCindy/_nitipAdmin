4a13ca5dc16ac00cfd30e6fd2f4e19ba1ac88778
CindoddCindy <cindodcindy@gmail.com>
2021-04-23 16:51:45 +0700

    buat layout admin view detail

---
+++ nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/ConfirmPaymentFromBuyerActivity.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.nitipadmin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.nitipadmin.R;
+
+public class ConfirmPaymentFromBuyerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_payment_from_buyer);
+    }
+}
