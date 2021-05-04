cee65217882de10a94c834231dc02dd24b5de6a8
CindoddCindy <cindodcindy@gmail.com>
2021-05-04 14:27:37 +0700

    tambah paymetn detail di activity payemtn detail

--- nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/AdminPaymentDetailActivity.java
+++ nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/AdminPaymentDetailActivity.java
@@ -26,7 +26,7 @@ public class AdminPaymentDetailActivity extends AppCompatActivity {
     private TextView textView_by_asal, textView_by_tujuan, textView_by_pengirim, textView_by_penerima, textView_by_jenis_barang, textView_by_berat_brg;
 
     //data barang di terima
-    private TextView textView_nama_penerima_barang_ar, textView_tanggal_terima_barang_arr,
+    private TextView textView_nama_penerima_brgArr, textView_tanggal_terima_barang_arr,
             textView_lokasi_terima_barang_arr;
 
     private TextView textView_add_pay_nama_pengirim, textView_add_pay_jumlah_uang, textView_add_pay_nama_bank, textView_add_pay_tanggal_kirim;
@@ -61,24 +61,61 @@ public class AdminPaymentDetailActivity extends AppCompatActivity {
         textView_by_jenis_barang=findViewById(R.id.tv_det_add_pay_by_jenis_barang);
         textView_by_berat_brg=findViewById(R.id.tv_det_add_pay_by_berat_barang);
 
-        //pengirim
-        textView_by_nama_pengirim=findViewById(R.id.tv_det_add_pay_by_tf_nama_akun);
-        textView_by_tgal_kirim=findViewById(R.id.tv_det_add_pay_by_tf_tgal_pay_jasa);
-        textView_by_jumlah_kirim=findViewById(R.id.tv_det_add_pay_by_tf_jumlah_uang);
-        textView_by_nama_bank=findViewById(R.id.tv_det_add_pay_by_tf_nama_bank_kirim);
-
-        //penerima
-        textView_ad_nama_pengirim=findViewById(R.id.tv_det_add_pay_ad_nama_terima);
-        textView_ad_tgal_kirim=findViewById(R.id.tv_det_add_pay_ad_tgal_terima);
-        textView_ad_jumlah_kirim=findViewById(R.id.tv_det_add_pay_ad_jumlah_uang_terima);
-        textView_ad_nama_bank=findViewById(R.id.tv_det_add_pay_ad_nama_bank_terima);
 
         textView_add_pay_nama_pengirim=findViewById(R.id.tv_det_add_pay_ad_tf_nama_penerima);
         textView_add_pay_jumlah_uang=findViewById(R.id.tv_det_add_pay_ad_tf_jumlah_uang);
         textView_add_pay_nama_bank=findViewById(R.id.tv_det_add_pay_ad_tf_nama_bank);
         textView_add_pay_tanggal_kirim=findViewById(R.id.tv_det_add_pay_ad_tf_tgal_di_tf);
 
+        textView_nama_penerima_brgArr=findViewById(R.id.tv_det_add_pay_brg_arr_nama_penerima);
+        textView_tanggal_terima_barang_arr=findViewById(R.id.tv_det_add_pay_brg_arr_tgal_diterima);
+        textView_lokasi_terima_barang_arr=findViewById(R.id.tv_det_add_pay_brg_arr_lokasi_diterima);
+
         textView_edit_bayar_seller=findViewById(R.id.tv_det_add_pay_edit_payment_btn);
 
     }
+
+    public void getDetailPayment(){
+
+        if(getIntent().getExtras()!=null) {
+            /**
+             * Jika Bundle ada, ambil data dari Bundle
+             */
+            Bundle bundle = getIntent().getExtras();
+
+            textView_add_pay_nama_pengirim.setText(bundle.getString("namaPenerimaUang"));
+            textView_add_pay_jumlah_uang.setText(bundle.getString("jumlahUang"));
+            textView_add_pay_nama_bank.setText(bundle.getString("bankTf"));
+            textView_add_pay_tanggal_kirim.setText(bundle.getString("tanggalTf"));
+
+
+            textView_nama_penerima_brgArr.setText(bundle.getString("namaPenerimaBarang"));
+            textView_tanggal_terima_barang_arr.setText(bundle.getString("tanggalBarangTiba"));
+            textView_lokasi_terima_barang_arr.setText(bundle.getString("lokasiBarangDiterima"));
+
+
+            // spHandle.setSpIdConfirmOrderEdit(SpHandle.SP_ID_CONFIRM_ORDER_EDIT, bundle.getLong("id_customer"));
+            //spHandle.setSpIdConfirmOrder(SpHandle.SP_ID_CONFIRM_ORDER,bundle.getLong("id_confirm"));
+            textView_asal.setText(bundle.getString("asal"));
+            textView_tujuan.setText(bundle.getString("tujuan"));
+            textView_date_going.setText(bundle.getString("tglgo"));
+            textView_date_arive.setText(bundle.getString("tglarr"));
+            textView_time_going.setText(bundle.getString("jamgo"));
+            textView_time_arrive.setText(bundle.getString("jamarr"));
+            textView_nama_penjual.setText(bundle.getString("namapenjual"));
+            textView_harga.setText(bundle.getString("harga"));
+            textView_kapasitas.setText(bundle.getString("kapasitas"));
+            textView_jenis_barang.setText(bundle.getString("jenisbarang"));
+
+            textView_by_asal.setText(bundle.getString("asalBr"));
+            textView_by_tujuan.setText(bundle.getString("tujuanBr"));
+            textView_by_pengirim.setText(bundle.getString("pengirim"));
+            textView_by_penerima.setText(bundle.getString("penerima"));
+            textView_by_jenis_barang.setText(bundle.getString("jenisBr"));
+            textView_by_berat_brg.setText(bundle.getString("beratBr"));
+
+
+        }
+
+        }
 }
--- nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/adapter/AdminPaymentAdapter.java
+++ nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/adapter/AdminPaymentAdapter.java
@@ -1,5 +1,9 @@
 package com.cindodcindy.nitipadmin.view.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,18 +13,81 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cindodcindy.nitipadmin.R;
+import com.cindodcindy.nitipadmin.model.pojo_money.pojo_get_money.Content;
+import com.cindodcindy.nitipadmin.view.SellerDoneDetailActivity;
+
+import java.util.List;
 
 
 public class AdminPaymentAdapter extends RecyclerView.Adapter<AdminPaymentAdapter.AdminPaymentChild> {
 
+    public List<com.cindodcindy.nitipadmin.model.pojo_money.pojo_get_money.Content> contentList;
+    public Context context;
+
+    public  AdminPaymentAdapter(List<Content> contentList, Context context){
+        this.contentList=contentList;
+        this.context=context;
+
+    }
+
     @NonNull
     @Override
     public AdminPaymentChild onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.admin_pay_to_seller_item, parent, false);
+        AdminPaymentChild mViewHolder = new AdminPaymentChild(mView);
+        return mViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdminPaymentChild holder, int position) {
+
+        final Content content= contentList.get(position);
+        holder.textView_nama_penerima.setText(content.getAnPenerima());
+        holder.textView_jumlah_uang.setText(content.getJumlahUang());
+        holder.textView_nama_bank.setText(content.getKeBank());
+        holder.textView_tanggal_dikirim.setText(content.getTanggalTransfer());
+
+        holder.cardView_admin_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putLong("id_seller", content.getIdSeller());
+                bundle.putString("asal",content.getAsal());
+                bundle.putString("tujuan",content.getTujuan());
+                bundle.putString("tglgo",content.getTanggalBerangkat());
+                bundle.putString("tglarr",content.getTanggalTiba());
+                bundle.putString("jamgo", content.getJamBerangkat());
+                bundle.putString("jamarr",content.getJamTiba());
+                bundle.putString("namapenjual",content.getNamaPenjual());
+                bundle.putString("kapasitas",content.getKapasitas());
+                bundle.putString("jenisbarang",content.getJenisBarang());
+                bundle.putString("harga", content.getHargaBagasi());
+
+                bundle.putString("asalBr",content.getAlamatPembeli());
+                bundle.putString("tujuanBr",content.getAlamatPenerima());
+                bundle.putString("pengirim",content.getNamaPembeli());
+                bundle.putString("penerima",content.getNamaPenerima());
+                bundle.putString("jenisBr", content.getJenisBarangKirim());
+                bundle.putString("beratBr",content.getKapasitasBarang());
+
+                bundle.putString("namaPenerimaUang",content.getAnPenerima());
+                bundle.putString("jumlahUang",content.getJumlahUang());
+                bundle.putString("bankTf",content.getKeBank());
+                bundle.putString("tanggalTf",content.getTanggalTransfer());
+
+
+                bundle.putString("tanggalBarangTiba",content.getTglBrgArr());
+                bundle.putString("namaPenerimaBarang",content.getAnBrgArr());
+                bundle.putString("lokasiBarangDiterima",content.getLocBrgArr());
+
+                Intent intent = new Intent(context, SellerDoneDetailActivity.class);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+
+            }
+        });
 
     }
 
