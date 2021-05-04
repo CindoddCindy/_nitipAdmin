d67361b697b9a57e7ae59ee31f7589765a1954de
CindoddCindy <cindodcindy@gmail.com>
2021-05-04 11:08:09 +0700

    rubah lagi di admin

--- nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/SellerDoneDetailActivity.java
+++ nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/SellerDoneDetailActivity.java
@@ -73,4 +73,42 @@ public class SellerDoneDetailActivity extends AppCompatActivity {
         textView_btn_bayar_seller=findViewById(R.id.tv_sell_don_btn_bayar_seller);
 
     }
+
+
+    public void getDataDetailDone(){
+        if(getIntent().getExtras()!=null) {
+            /**
+             * Jika Bundle ada, ambil data dari Bundle
+             */
+            Bundle bundle = getIntent().getExtras();
+
+            // spHandle.setSpIdConfirmOrderEdit(SpHandle.SP_ID_CONFIRM_ORDER_EDIT, bundle.getLong("id_customer"));
+            //spHandle.setSpIdConfirmOrder(SpHandle.SP_ID_CONFIRM_ORDER,bundle.getLong("id_confirm"));
+            textView_asal.setText(bundle.getString("asal"));
+            textView_tujuan.setText(bundle.getString("tujuan"));
+            textView_date_going.setText(bundle.getString("tglgo"));
+            textView_date_arive.setText(bundle.getString("tglarr"));
+            textView_time_going.setText(bundle.getString("jamgo"));
+            textView_time_arrive.setText(bundle.getString("jamarr"));
+            textView_nama_penjual.setText(bundle.getString("namapenjual"));
+            textView_harga.setText(bundle.getString("harga"));
+            textView_kapasitas.setText(bundle.getString("kapasitas"));
+            textView_jenis_barang.setText(bundle.getString("jenisbarang"));
+
+            textView_by_asal.setText(bundle.getString("asalBr"));
+            textView_by_tujuan.setText(bundle.getString("tujuanBr"));
+            textView_by_pengirim.setText(bundle.getString("pengirim"));
+            textView_by_penerima.setText(bundle.getString("penerima"));
+            textView_by_jenis_barang.setText(bundle.getString("jenisBr"));
+            textView_by_berat_brg.setText(bundle.getString("beratBr"));
+
+            textView_diterima_oleh.setText(bundle.getString("namaPenerimaBarang"));
+            textView_tgal_diterima.setText(bundle.getString("tanggalBarangTiba"));
+            textView_lokasi_get_brg.setText(bundle.getString("lokasiBarangDiterima"));
+
+
+
+
+        }
+    }
 }
--- nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/adapter/SellerDoneAdapter.java
+++ nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/adapter/SellerDoneAdapter.java
@@ -1,5 +1,9 @@
 package com.cindodcindy.nitipadmin.view.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,17 +13,70 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cindodcindy.nitipadmin.R;
+import com.cindodcindy.nitipadmin.model.pojo_done.pojo_get_done.Content;
+import com.cindodcindy.nitipadmin.view.SellerDoneDetailActivity;
+
+import java.util.List;
 
 public class SellerDoneAdapter extends RecyclerView.Adapter<SellerDoneAdapter.SellerDoneChild> {
+
+    public List<Content> contentList;
+    public Context context;
+
+    public  SellerDoneAdapter(List<Content> contentList, Context context){
+        this.contentList=contentList;
+        this.context=context;
+
+    }
 
     @NonNull
     @Override
     public SellerDoneChild onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.done_item_layout, parent, false);
+        SellerDoneChild mViewHolder = new SellerDoneChild(mView);
+        return mViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull SellerDoneChild holder, int position) {
+        final Content content= contentList.get(position);
+        holder.textView_nama_penerima.setText(content.getDiterimaOleh());
+        holder.textView_tgal_diterimma.setText(content.getTanggalBarangTiba());
+
+        holder.cardView_seller_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putLong("id_seller", content.getIdSeller());
+                bundle.putString("asal",content.getAsal());
+                bundle.putString("tujuan",content.getTujuan());
+                bundle.putString("tglgo",content.getTanggalBerangkat());
+                bundle.putString("tglarr",content.getTanggalTiba());
+                bundle.putString("jamgo", content.getJamBerangkat());
+                bundle.putString("jamarr",content.getJamTiba());
+                bundle.putString("namapenjual",content.getNamaPenjual());
+                bundle.putString("kapasitas",content.getKapasitas());
+                bundle.putString("jenisbarang",content.getJenisBarang());
+                bundle.putString("harga", content.getHargaBagasi());
+
+                bundle.putString("asalBr",content.getAlamatPembeli());
+                bundle.putString("tujuanBr",content.getAlamatPenerima());
+                bundle.putString("pengirim",content.getNamaPembeli());
+                bundle.putString("penerima",content.getNamaPenerima());
+                bundle.putString("jenisBr", content.getJenisBarangKirim());
+                bundle.putString("beratBr",content.getKapasitasBarang());
+
+                bundle.putString("tanggalBarangTiba",content.getTanggalBarangTiba());
+                bundle.putString("namaPenerimaBarang",content.getDiterimaOleh());
+                bundle.putString("lokasiBarangDiterima",content.getLokasiBarangDiterima());
+
+                Intent intent = new Intent(context, SellerDoneDetailActivity.class);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
+
+            }
+        });
 
     }
 
