e021a3226f06de241fcb4c15f351d54fa22d09bb
CindoddCindy <cindodcindy@gmail.com>
2021-05-05 13:03:59 +0700

    tambah admin btm nav

--- nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/BottomNav.java
+++ nitipAdminApps/app/src/main/java/com/cindodcindy/nitipadmin/view/BottomNav.java
@@ -1,10 +1,18 @@
 package com.cindodcindy.nitipadmin.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.cindodcindy.nitipadmin.R;
+import com.cindodcindy.nitipadmin.view.fragment.AdminConfirmBeforeDoneFragment;
+import com.cindodcindy.nitipadmin.view.fragment.AdminSendPaymentFragment;
+import com.cindodcindy.nitipadmin.view.fragment.BuyerPaymentFragment;
+import com.cindodcindy.nitipadmin.view.fragment.SellerDoneFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BottomNav extends AppCompatActivity {
 
@@ -12,5 +20,53 @@ public class BottomNav extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_nav);
+
+        //Menampilkan halaman Fragment yang pertama kali muncul
+        getFragmentPage(new BuyerPaymentFragment());
+
+        /*Inisialisasi BottomNavigationView beserta listenernya untuk
+         *menangkap setiap kejadian saat salah satu menu item diklik
+         */
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+
+                //Menantukan halaman Fragment yang akan tampil
+                switch (item.getItemId()){
+                    case R.id.buyer_payment:
+                        fragment = new BuyerPaymentFragment();
+                        break;
+
+                    case R.id.confirm_to_seller:
+                        fragment = new AdminConfirmBeforeDoneFragment();
+                        break;
+
+                    case R.id.seller_done:
+                        fragment = new SellerDoneFragment();
+                        break;
+
+                    case R.id.admin_payment:
+                        fragment = new AdminSendPaymentFragment();
+                        break;
+                }
+                return getFragmentPage(fragment);
+            }
+        });
+    }
+
+
+    //Menampilkan halaman Fragment
+    private boolean getFragmentPage(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.page_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
